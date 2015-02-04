a7d7b6e929739fb76aa9619dd6d522cb46ddc2b5
Sharon Henderson <schender@esf.edu>
2015-02-04 13:59:38 -0500

    More tuple updates, link to file changes

--- FPIMV2/Models/TupleModelBinder.cs
+++ FPIMV2/Models/TupleModelBinder.cs
@@ -21,105 +21,112 @@ namespace FPIMV2.Models
         protected override void BindProperty(ControllerContext controllerContext, ModelBindingContext bindingContext,
 System.ComponentModel.PropertyDescriptor propertyDescriptor)
         {
-            if (propertyDescriptor.Name == "ProfileId")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item2.ProfileId"]);
-                return;
-            }
+            string actionName = controllerContext.RouteData.Values["action"].ToString();
 
-            if (propertyDescriptor.Name == "UserId")
+            // Only check views with Tuples
+            if ((actionName == "ManuallyDeleteFaculty") || (actionName == "ManuallyUpdateFaculty") || (actionName == "ManuallyAddFaculty") || (actionName == "ManageNonHRFaculty"))
             {
-               propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.UserId"]);
-               return;
-            }
-            if (propertyDescriptor.Name == "FirstName")
-            {
-           //     DateTime dt = new DateTime(int.Parse(controllerContext.HttpContext.Request.Form["year"]),
-           // int.Parse(controllerContext.HttpContext.Request.Form["month"]),
-           // int.Parse(controllerContext.HttpContext.Request.Form["day"]));
-             //   propertyDescriptor.SetValue(bindingContext.Model, dt);
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.FirstName"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "LastName")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.LastName"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "MiddleName")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.MiddleName"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "Suffix")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.Suffix"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "OffcBldg")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.OffcBldg"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "OffcRoom")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.OffcRoom"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "OffcPhone")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.OffcPhone"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "SpeedDialExt")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.SpeedDialExt"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "MailAddrBldg")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.MailAddrBldg"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "MailAddrRoom")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.MailAddrRoom"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "AltOffcPhone")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.AltOffcPhone"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "EmailId")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.EmailId"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "CampusTitle")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.CampusTitle"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "WorkLoc")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.WorkLoc"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "CivilServiceTitle")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.CivilServiceTitle"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "ESFAD")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.ESFAD"]);
-                return;
-            }
-            if (propertyDescriptor.Name == "SUAD")
-            {
-                propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.SUAD"]);
-                return;
+
+                if (propertyDescriptor.Name == "ProfileId")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item2.ProfileId"]);
+                    return;
+                }
+
+                if (propertyDescriptor.Name == "UserId")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.UserId"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "FirstName")
+                {
+                    //     DateTime dt = new DateTime(int.Parse(controllerContext.HttpContext.Request.Form["year"]),
+                    // int.Parse(controllerContext.HttpContext.Request.Form["month"]),
+                    // int.Parse(controllerContext.HttpContext.Request.Form["day"]));
+                    //   propertyDescriptor.SetValue(bindingContext.Model, dt);
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.FirstName"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "LastName")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.LastName"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "MiddleName")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.MiddleName"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "Suffix")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.Suffix"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "OffcBldg")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.OffcBldg"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "OffcRoom")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.OffcRoom"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "OffcPhone")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.OffcPhone"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "SpeedDialExt")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.SpeedDialExt"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "MailAddrBldg")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.MailAddrBldg"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "MailAddrRoom")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.MailAddrRoom"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "AltOffcPhone")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.AltOffcPhone"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "EmailId")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.EmailId"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "CampusTitle")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.CampusTitle"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "WorkLoc")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.WorkLoc"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "CivilServiceTitle")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.CivilServiceTitle"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "ESFAD")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.ESFAD"]);
+                    return;
+                }
+                if (propertyDescriptor.Name == "SUAD")
+                {
+                    propertyDescriptor.SetValue(bindingContext.Model, controllerContext.HttpContext.Request.Form["Item1.SUAD"]);
+                    return;
+                }
             }
 
             base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
